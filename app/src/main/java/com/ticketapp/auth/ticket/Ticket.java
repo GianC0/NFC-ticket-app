eee07475ee6e629d2c9cb042c2dca220e3dbdefe
Giancarlo Andriano <giancarlo.andriano3@gmail.com>
2022-12-14 18:14:17 +0200

    issue function implemented
    TODO: change authKey without datetime

--- app/src/main/java/com/ticketapp/auth/ticket/Ticket.java
+++ app/src/main/java/com/ticketapp/auth/ticket/Ticket.java
@@ -5,7 +5,23 @@ import com.ticketapp.auth.app.main.TicketActivity;
 import com.ticketapp.auth.app.ulctools.Commands;
 import com.ticketapp.auth.app.ulctools.Utilities;
 
+import java.nio.charset.StandardCharsets;
 import java.security.GeneralSecurityException;
+
+
+import java.nio.ByteBuffer;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * TODO:
@@ -19,11 +35,6 @@ public class Ticket {
     private static final byte[] defaultAuthenticationKey = TicketActivity.outer.getString(R.string.default_auth_key).getBytes();
     private static final byte[] defaultHMACKey = TicketActivity.outer.getString(R.string.default_hmac_key).getBytes();
 
-    /** TODO: Change these according to your design. Diversify the keys. */
-    private static final byte[] authenticationKey = defaultAuthenticationKey; // 16-byte key
-    private static final byte[] hmacKey = defaultHMACKey; // 16-byte key
-
-    public static byte[] data = new byte[192];
 
     private static TicketMac macAlgorithm; // For computing HMAC over ticket data, as needed
     private static Utilities utils;
@@ -35,24 +46,11 @@ public class Ticket {
 
     private static String infoToShow = "-"; // Use this to show messages
 
-
-    // utility function for padding to 4-bytes-size messages
-    public byte[] ParseTo4Bytes(String text){
-        byte[] text_bytes = text.getBytes();
-        byte[] out = new byte[text_bytes.length + text_bytes.length%4];
-        for (int i=0; i<text_bytes.length; i++) {
-            out[i] = text_bytes[i];
-        }
-        return out;
-    }
-
-
-
     /** Create a new ticket */
     public Ticket() throws GeneralSecurityException {
         // Set HMAC key for the ticket
         macAlgorithm = new TicketMac();
-        macAlgorithm.setKey(hmacKey);
+        macAlgorithm.setKey(hmacMasterKey);
 
         ul = new Commands();
         utils = new Utilities(ul);
@@ -78,40 +76,189 @@ public class Ticket {
         return infoToShow;
     }
 
+
+    // -------------------- NEW FUNCTIONS-----------------
+    public static byte[] keyGen (int length, String algo) {
+        try{
+            KeyGenerator generator = KeyGenerator.getInstance(algo);
+            generator.init(length);
+            byte[] out = generator.generateKey().getEncoded();
+            return out;
+        }catch (NoSuchAlgorithmException e){
+            infoToShow = "Non existent encrypton algorithm";
+            return null;
+        }
+
+    }
+
+    /** generates a 128-bit-truncated SHA256 from input string */
+    public static byte[] getTruncatedSHA(byte[] input) throws NoSuchAlgorithmException {
+        // Static getInstance method is called with hashing SHA
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+
+        // digest() method called to calculate message digest of an input
+        byte[] sha256 = md.digest(input);
+
+        //truncation to 128-bit
+        return Arrays.copyOf(sha256,16);
+    }
+
+    /** concatenates two arrays of bytes */
+    public static byte[] concatByteArrays(byte[] array1, byte[] array2) {
+        int length = array1.length + array2.length;
+
+        byte[] result = new byte[length];
+        int pos = 0;
+        for (byte element : array1) {
+            result[pos] = element;
+            pos++;
+        }
+
+        for (byte element : array2) {
+            result[pos] = element;
+            pos++;
+        }
+
+        return result;
+
+    }
+
+    /** gets integer from byte array */
+    public static int byteToInt(byte[] byte_array){
+        int value = 0;
+        for (byte b : byte_array) {
+            value = (value << 8) + (b & 0xFF);
+        }
+        return value;
+    }
+
+    /** merges byte arrays for final data */
+    public static byte[] mergeData(byte[] hmacKey, byte[] exp_date, byte[] max_counter,byte[] init_counter, byte[] tag, byte[] vers){
+        return concatByteArrays(hmacKey,
+                concatByteArrays(exp_date,
+                        concatByteArrays(max_counter,
+                                concatByteArrays(init_counter,
+                                        concatByteArrays(tag,vers)))));
+    }
+
+    // ------------------------------------------------------
+
+    /** TODO: Change these according to your design. Diversify the keys. */
+    private static byte[] authenticationMasterKey = keyGen(128,"AES"); // 16-byte key
+    private static byte[] hmacMasterKey = keyGen(128,"HmacSHA1"); // 16-byte key
+
+
+
     /**
      * Issue new tickets
      *
      * TODO: IMPLEMENT
      */
+
     public boolean issue(int daysValid, int uses) throws GeneralSecurityException {
         boolean res;
 
+        byte[] old_authKey = new byte[4];
+        utils.readPages(44,4,old_authKey,0);
+        Utilities.log("Card was used before: trying to authenticate first then delete", false);
+
         // Authenticate
-        res = utils.authenticate(authenticationKey);
+        res = utils.authenticate(old_authKey);
         if (!res) {
             Utilities.log("Authentication failed in issue()", true);
             infoToShow = "Authentication failed";
             return false;
         }
+        utils.eraseMemory();
 
-        // Example of writing:
 
-        byte[] message = ParseTo4Bytes("Shuto's mum is a good woman");
-        utils.writePages(message, 0, 6, message.length/4);
+        // initializing cipher for encryption
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
 
-        //Counter writing
-        byte[] counter_increment = {(byte) 5,(byte) 0,(byte) 0, (byte) 0};
-        res = utils.writePages(counter_increment, 0, 41, 1);
+
+        /** CARD AUTH KEY SECTION */
+        // computing card authKey
+        byte[] tmp = new byte[12];
+        utils.readPages(0,3,tmp,0);
+        byte[] UID = Arrays.copyOf(tmp,9);
+        byte[] timestamp = (new Date()).toString().getBytes();
+        byte[] authKey = getTruncatedSHA(concatByteArrays((concatByteArrays(authenticationMasterKey, UID)) , timestamp));   // 16 bytes -> 4 pages
+
+        // encrypting and storing card authKey
+        cipher.init(Cipher.ENCRYPT_MODE,new SecretKeySpec(authenticationMasterKey, "AES"));
+        byte[] authKey_encrypted = cipher.doFinal(authKey);
+        utils.writePages(authKey_encrypted, 0, 44, 4);
+        Utilities.log("encrypted authKey saved correctly",false);
+
+
+
+        /**  DATA SECTION */
+        // hmacKey calculation
+        byte[] hmacKey = getTruncatedSHA(concatByteArrays((concatByteArrays(hmacMasterKey, UID)) , timestamp));
+
+
+        // get initial state of counter and calculating max value
+        byte[] counter_initial_bytes = new byte[4];     // 4 bytes -> 1 page
+        utils.readPages(41,1, counter_initial_bytes,0);
+        int counter_max_int = uses + byteToInt(counter_initial_bytes);
+        if (counter_max_int>65535) {
+            infoToShow = "ERROR! Counter exceeded MAX value: need new card";
+            Utilities.log("Counter exceeded max value", true);
+            return false;
+        }
+        byte[] counter_max_bytes = ByteBuffer.allocate(4).putInt(counter_max_int).array();
+        counter_max_bytes = Arrays.copyOfRange(counter_max_bytes,2,4);
+        counter_max_bytes = Arrays.copyOf(counter_max_bytes,4);                // 4 bytes -> 1 page
+
+
+        // calculate expiry date
+        Date currentDate = new Date();
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(currentDate);
+        calendar.add(Calendar.DAY_OF_MONTH, daysValid);
+        byte[] expiry_date = calendar.getTime().toString().getBytes();
+        expiry_date = Arrays.copyOf(expiry_date,36);             // 34+2 bytes -> 9 pages
+
+        // application tag and version number
+        byte[] tag = "NetS".getBytes();  // 4 bytes -> 1 page
+        byte[] version = {(byte) 0,(byte) 0,(byte) 0,(byte) 1};  // 4 bytes -> 1 page
+
+
+        /** ENCRYPTION SECTION */
+        byte[] data = mergeData(        //  TOT = 17 pages
+                hmacKey,                // 4 pages
+                expiry_date,            // 9 pages
+                counter_max_bytes,      // 1 page
+                counter_initial_bytes,  // 1 page
+                tag,                    // 1 page
+                version);               // 1 page
+        cipher.init(Cipher.ENCRYPT_MODE,new SecretKeySpec(authKey, "AES"));
+        byte[] data_encrypted = cipher.doFinal(data);
+        utils.writePages(data_encrypted, 0, 4, 17);
+        Utilities.log("encrypted data saved correctly",false);
+
+        /**  HMAC SECTION */
+        macAlgorithm.setKey(hmacKey);
+        byte[] HMAC = macAlgorithm.generateMac(data_encrypted);
+        utils.writePages(HMAC, 0, 21, 5);
+        Utilities.log("hmac saved correctly",false);
+
+
+        /** AUTH0 and AUTH1  */
+        byte[] AUTH0 = {(byte) 3,(byte) 0,(byte) 0,(byte) 0};
+        byte[] AUTH1 = {(byte) 1,(byte) 0,(byte) 0,(byte) 0};
+        utils.writePages(AUTH0, 0, 42, 1);
+        utils.writePages(AUTH1, 0, 43, 1);
+        Utilities.log("AUTHx saved correctly",false);
+
+        //TODO: LOCKBITS or other errors checks
 
         // Set information to show for the user
-        if (res) {
-            infoToShow = "Wrote: " + new String(message);
-        } else {
-            infoToShow = "Failed to write";
-        }
+        infoToShow = "ISSUE is successful";
 
         return true;
     }
+
 
     /**
      * Use ticket once
@@ -122,7 +269,7 @@ public class Ticket {
         boolean res;
 
         // Authenticate
-        res = utils.authenticate(authenticationKey);
+        res = utils.authenticate(authenticationMasterKey);
         if (!res) {
             Utilities.log("Authentication failed in issue()", true);
             infoToShow = "Authentication failed";
@@ -131,12 +278,7 @@ public class Ticket {
 
         // Example of reading:
         byte[] message = new byte[4];
-        utils.readPages(6, 1, message, 0);
-
-
-        //Counter increment
-        byte[] counter_increment = {(byte) 1,(byte) 0,(byte) 0, (byte) 0};
-        res = utils.writePages(counter_increment, 0, 41, 1);
+        res = utils.readPages(6, 1, message, 0);
 
         // Set information to show for the user
         if (res) {
